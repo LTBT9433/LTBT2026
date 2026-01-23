7347b1d1eea9ec4682c67e3771164e7b12cc1a2f
LTBT9433 <ashdalerobotics@gmail.com>
2026-01-23 13:47:10 +0800

    comment out Navx3 and add todo

--- LTBT2026/src/main/java/frc/robot/RobotContainer.java
+++ LTBT2026/src/main/java/frc/robot/RobotContainer.java
@@ -37,6 +37,7 @@ public class RobotContainer
   // Replace with CommandPS4Controller or CommandJoystick if needed
   final         CommandXboxController driverXbox = new CommandXboxController(0);
   // The robot's subsystems and commands are defined here...
+  // TODO: update the config to use our configs instead
   private final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
                                                                                 "swerve/neo"));
 
